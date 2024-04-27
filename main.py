2c83cff1ee8bc366f13eefc74663ec777439791f
saklain nizam thakur <54598229+Nizamthakur@users.noreply.github.com>
2024-04-28 04:13:08 +0600

    render the images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -18,3 +19,19 @@ I am Saklain Nizam Thakur, a Python developer. I graduated in 2024 and have been
     st.info(content)
 st.markdown("""
 Below you can find some of the apps that I have build in python recently. Feel  free to contact me!""")
+
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("data.csv", sep = ";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4 :
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
+
+
