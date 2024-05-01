d3564dba735c229d056a7e078a3c00fada7d8d46
saklain nizam thakur <54598229+Nizamthakur@users.noreply.github.com>
2024-05-02 01:29:26 +0600

    sent emails using the forms

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,10 +1,18 @@
 import streamlit as st
+from send_email import send_email
 st.header("contact us")
-with  st.form(key ="user_email"):
+with st.form(key ="user_email"):
     user_email = st.text_input("Your Email Address")
-    massage = st.text_area("Your massage Here")
-    button = st.form_submit_button ("submit")
-
-    if button :
-        massage = massage + user_email
-        
+    raw_massage = st.text_area("Your massage Here")
+    subject = f"New email from {user_email}"
+    message = f"""\
+            Subject: {subject}\n
+            From: {user_email}\n
+            {raw_massage}
+            """
+    button = st.form_submit_button("submit")
+    print(button)
+    if button:
+        send_email(message)
+        st.info("your email was sent sucesfully ")
+        # massage = massage + user_email
