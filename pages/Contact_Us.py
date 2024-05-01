4c6c2b7d3aef727c3df942cda44a2ccde59f9f87
saklain nizam thakur <54598229+Nizamthakur@users.noreply.github.com>
2024-05-02 01:08:33 +0600

    email sent

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,2 +1,10 @@
 import streamlit as st
 st.header("contact us")
+with  st.form(key ="user_email"):
+    user_email = st.text_input("Your Email Address")
+    massage = st.text_area("Your massage Here")
+    button = st.form_submit_button ("submit")
+
+    if button :
+        massage = massage + user_email
+        
